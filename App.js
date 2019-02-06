de5de7dce9feedd7081163527112c08465237081
Pavel-Demidyuk <3128914@gmail.com>
2019-02-06 14:13:42 +0300

    adding some comments

--- App.js
+++ App.js
@@ -10,6 +10,9 @@ export default class App extends React.Component {
     fontLoaded: false,
   };
 
+
+// some comments
+
   async componentDidMount() {
     await Font.loadAsync({
       'mega-font': require('./assets/MajorMonoDisplay-Regular.ttf'),
