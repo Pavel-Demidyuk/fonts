f8cdacf228ef09733fc4cc3b2798195fd06f551b
Pavel-Demidyuk <3128914@gmail.com>
2019-02-06 14:11:55 +0300

    test commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Font } from 'expo';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'mega-font': require('./assets/MajorMonoDisplay-Regular.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
+  render() {
+    if (this.state.fontLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontFamily: 'mega-font', fontSize: 56 }}>
+        Hello, world!
+        </Text>
+        </View>
+      );
+    }
+    else {
+      return (<Text>Waiting for font</Text>)
+    }
+  }
+}
